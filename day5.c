4e6a4162baf3cb85a92bc665cb7603aa906e11a2
21341A05i5_Avinash <97654811+Chintu004@users.noreply.github.com>
2024-06-29 12:02:03 +0530

    Add files via upload

---
+++ day5.c
@@ -0,0 +1,80 @@
+// #include<stdio.h>
+// int main(){
+//     int x,grade;
+//     scanf("%d",&x);
+//     grade=x/10;
+//     switch (grade)
+//     {
+//     case 10:
+//     case 9:
+//     printf("Grade:A");
+//     break;
+//     case 8:
+//     printf("Grade:B");
+//     break;
+//     case 7:
+//     printf("Grade:C");
+//     break;
+//     case 6:
+//     printf("Grade:D");
+//     break;
+//     default:
+//     printf("Fail");
+//     }
+// }
+
+
+
+// #include<stdio.h>
+// int main(){
+//     int x,y,z,q,options,balance=1000;
+//     printf("1.Check Balance\n2.Deposite Money\n3.Withdraw Money\n4.Exit\n");
+//     printf("Enter the options:");
+//     scanf("%d",&options);
+//     switch (options)
+//     {
+//     case 0:
+//     case 1:
+//     printf("Your Balance is %d",balance);
+//     break;
+//     case 2:
+//     printf("Enter the amount to deposit:");
+//     scanf("%d",&x);
+//     y=balance+x;
+//     printf("your balance is:%d",y);
+//     break;
+//     case 3:
+//     printf("Enter the amount to withdraw:");
+//     scanf("%d",&z);
+//     q=balance-z;
+//     printf("your balance is:%d\n",q);
+//     if (z>balance)
+//     {
+//         printf("Insufficient Balance");
+//     }
+//     break;
+//     case 4:
+//     printf("Thank you");
+//     break;
+//     default:
+//     printf("Invalid option");
+//         break;
+//     }
+// }
+
+//Cheap Codeforces
+// #include<stdio.h>
+// int main(){
+//     int n,m,a,b;
+//     scanf("%d %d %d %d",&n,&m,&a,&b);
+//     if(m*a<b){
+//         printf("%d",n*a);
+
+//     }
+//     else{
+//         printf("%d",(n/m)*b+((n%m)*a<b?(n%m)*a:b));
+//     }
+//     return 0;
+// }
+
+
