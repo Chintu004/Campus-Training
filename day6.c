98ffe1ed97846dfd76fcc828ddd50d03d844d1cc
21341A05i5_Avinash <97654811+Chintu004@users.noreply.github.com>
2024-07-02 12:06:57 +0530

    Add files via upload

---
+++ day6.c
@@ -0,0 +1,67 @@
+// #include<stdio.h>
+// void main(){
+//     int a=0,b=2;
+//     if (a=0)
+//     b=0;
+//     else
+//     b*=10;
+//     printf("%d %d",a,b);
+// }
+
+
+// #include<stdio.h>
+// int main(){
+//     char c='a';
+//     switch (c)
+//     {
+//         case 97:
+//         break;
+//         case 'a':
+//         printf("98");
+//         break;
+//         case 99;
+//         printf("99");
+//         break;
+//         default:
+//         printf("default");
+//         break;
+//     }
+// }
+
+
+// #include<stdio.h>
+// int main(){
+//     int x=10;
+//     do{
+//         x++;
+
+//     }
+//     while (x++>12);
+//     {
+//         printf("%d",x);
+//     }
+//     return 0;
+// }
+
+
+
+// #include<stdio.h>
+// int main(){
+//     int a=1;
+//     printf("Size of the a is:%d\n",sizeof(++a));
+//     printf("Value of a is:%d",a);
+// }\#include<stdio.h>
+
+
+#include<stdio.h>
+int main(){
+    int c,a=25;
+    c=printf("%d",a);
+    printf("\n%d",c);
+    return 0;
+ }
+
+
+
+
+
